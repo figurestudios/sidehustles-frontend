6592250a9418080aec7a82f005519bd76043bbbb
figurestudios <figuregang@gmail.com>
2022-05-22 10:33:19 +0200

    add web3js / coinbase support

--- src/App.js
+++ src/App.js
@@ -2,8 +2,32 @@
 //import Content from "./Content"
 //import Banner from "./Banner"
 import { SkynetClient, genKeyPairFromSeed } from 'skynet-js';
+import { WalletLinkConnector } from "@web3-react/walletlink-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { useWeb3React } from '@web3-react/core'
+
+const CoinbaseWallet = new WalletLinkConnector({
+ url: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+ appName: "Web3-react Demo",
+ supportedChainIds: [1, 3, 4, 5, 42],
+});
+
+const WalletConnect = new WalletConnectConnector({
+ rpcUrl: `https://mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+ bridge: "https://bridge.walletconnect.org",
+ qrcode: true,
+});
+
+const Injected = new InjectedConnector({
+ supportedChainIds: [1, 3, 4, 5, 42]
+});
 
 function App(){
+
+  const { activate, deactivate } = useWeb3React();
+
+  const { active, chainId, account } = useWeb3React();
 
   const portal =
   window.location.hostname === 'localhost' ? 'https://siasky.net' : undefined;
@@ -12,6 +36,7 @@ function App(){
 
   const { publicKey } = genKeyPairFromSeed("this seed should be fairly long for security");
   const { privateKey } = genKeyPairFromSeed("this seed should be fairly long for security");
+
   const dataKey = "myApp";
   const json = { example: "Hello." };
 
@@ -56,6 +81,15 @@ function App(){
       <button onClick={getJSONExample}>Get JSON</button>
       <button onClick={setJSONExample}>Set JSON</button>
       <button onClick={getEntryExample}>Get URL</button>
+      <button onClick={() => { activate(CoinbaseWallet) }}>Coinbase Wallet</button>
+      <button onClick={() => { activate(WalletConnect) }}>Wallet Connect</button>
+      <button onClick={() => { activate(Injected) }}>Metamask</button>
+
+      <button onClick={deactivate}>Disconnect</button>
+
+      <div>{"Connection Status: "+active}</div>
+      <div>{"Account: "+account}</div>
+      <div>{"Network ID: "+chainId}</div>
     </div>
   )
 }
